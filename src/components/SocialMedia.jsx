b6b103bbc91bded887d4fe916413f03aeba5184c
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-07-31 09:15:33 +0200

    Header and social media

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,21 +1,72 @@
 import React from "react";
-import {BsTelegram, BsInstagram} from 'react-icons/bs'
-import {FaFacebookF} from 'react-icons/fa';
+import {
+  BsTelegram,
+  BsInstagram,
+  BsLinkedin,
+  BsWhatsapp,
+  BsGithub,
+} from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => {
-    return(
-        <div className="app__social">
-            <div>
-                <BsTelegram />
-            </div>
-            <div>
-                <FaFacebookF />
-            </div>
-            <div>
-                <BsInstagram />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="app__social">
+      <div>
+        <a
+          target={"_blank"}
+          rel="noreferrer"
+          href="https://telegram.me/ShYuldoshov"
+        >
+          <BsTelegram />
+        </a>
+      </div>
+      <div>
+        <a
+          target={"_blank"}
+          rel="noopener noreferrer"
+          href="https://www.facebook.com/shakhzodbek.yuldoshev/"
+        >
+          <FaFacebookF />
+        </a>
+      </div>
+      <div>
+        <a
+          target={"_blank"}
+          rel="noopener noreferrer"
+          href="https://www.instagram.com/shakhzodbek_yuldashev/"
+        >
+          <BsInstagram />
+        </a>
+      </div>
+      <div>
+        <a
+          target={"_blank"}
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/shakhzod-yuldoshov-5bb8aa190/"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
+      <div>
+        <a
+          target={"_blank"}
+          rel="noopener noreferrer"
+          href="https://wa.me/+48575829114"
+        >
+          <BsWhatsapp />
+        </a>
+      </div>
+      <div>
+        <a
+          target={"_blank"}
+          rel="noopener noreferrer"
+          href="https://github.com/shakhzodbek1997"
+        >
+          <BsGithub />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default SocialMedia;
